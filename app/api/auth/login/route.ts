f6ec4f9fe9e0dc2c5ba2f399053de1f8d135a99c
lihenko <obbdpu@gmail.com>
2025-05-21 19:33:20 +0300

    Smakota

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,9 +1,9 @@
 // /app/api/auth/login/route.ts
 
-import prisma from "../../../lib/prisma"; // Це залежить від вашої структури
+import prisma from "@/app/lib/prisma";
 import bcrypt from "bcryptjs";
 import { NextResponse } from "next/server";
-import * as jose from 'jose'
+import * as jose from 'jose';
 
 export async function POST(request: Request) {
   try {
@@ -13,7 +13,6 @@ export async function POST(request: Request) {
       return NextResponse.json({ error: "Email and password are required" }, { status: 400 });
     }
 
-    // Знайти користувача в базі за email
     const user = await prisma.user.findUnique({
       where: { email },
     });
@@ -22,22 +21,32 @@ export async function POST(request: Request) {
       return NextResponse.json({ error: "Невірний email або пароль" }, { status: 400 });
     }
 
-    // Перевірка пароля
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return NextResponse.json({ error: "Невірний email або пароль" }, { status: 400 });
     }
 
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET,);
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
     const alg = 'HS256';
-      
-      const jwt = await new jose.SignJWT({ role: user.role })
-        .setProtectedHeader({ alg })
-        .setExpirationTime('72h')
-        .setSubject(user.id.toString())
-        .sign(secret);
-    return Response.json({token: jwt});
-    
+
+    const jwt = await new jose.SignJWT({ role: user.role })
+      .setProtectedHeader({ alg })
+      .setExpirationTime('72h')
+      .setSubject(user.id.toString())
+      .sign(secret);
+
+    const response = NextResponse.json({ message: "Успішний вхід" });
+
+    response.cookies.set('Authorization', jwt, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production', // обов’язково для продакшну
+      sameSite: 'lax',
+      path: '/',
+      maxAge: 60 * 60 * 24 * 3, // 3 дні
+    });
+
+    return response;
+
   } catch (error) {
     console.error("Login error:", error);
     return NextResponse.json({ error: "Йой, щось пішло не так" }, { status: 500 });
