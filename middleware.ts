e9f40dde50e38e899106980bcc14b2782d9930fd
lihenko <obbdpu@gmail.com>
2025-05-21 19:28:30 +0300

    Smakota

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,26 @@
 // app/middleware.ts
-import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import * as jose from 'jose';
 
 export async function middleware(request: NextRequest) {
-  const cookie = (await cookies()).get('Authorization');
+  const jwt = request.cookies.get('Authorization')?.value;
 
-  if (!cookie) {
+  if (!jwt) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
 
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-  const jwt = cookie.value;
-
   try {
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
     const { payload } = await jose.jwtVerify(jwt, secret);
 
     // Обмеження доступу до /adminpanel/ тільки для role === 'admin'
     if (request.nextUrl.pathname.startsWith('/adminpanel')) {
-
       if (payload.role !== 'admin') {
-        return NextResponse.redirect(new URL('/unauthorized', request.url)); // створіть цю сторінку для повідомлення про брак доступу
+        return NextResponse.redirect(new URL('/unauthorized', request.url));
       }
     }
 
-    // Інакше — дозвіл на доступ
     return NextResponse.next();
 
   } catch (err) {
