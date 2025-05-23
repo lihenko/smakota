71a3c5ad82f45e338d9d24310c16b6e477f620a7
lihenko <obbdpu@gmail.com>
2025-05-23 16:48:10 +0300

    Smakota

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -4,12 +4,24 @@ import prisma from '../../lib/prisma';
 
 export async function GET() {
   try {
-    const users = await prisma.user.findMany(); // Отримуємо всіх користувачів
+    const users = await prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        slug: true,
+        avatar: {
+          select: {
+            avatarUrl: true,
+          },
+        },
+      },
+    });
+
     return NextResponse.json(users);
   } catch (error) {
     console.error(error);
     return NextResponse.json({ message: 'Error fetching users' }, { status: 500 });
   } finally {
-    await prisma.$disconnect();  // Закриваємо підключення після запиту
+    await prisma.$disconnect();
   }
 }
