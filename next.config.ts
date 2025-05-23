a222eb5d259ef01375e98aacf525d659c67bf9f5
lihenko <obbdpu@gmail.com>
2025-05-23 16:07:51 +0300

    Smakota

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-
+    images: {
+        domains: ['qxmpwcz3yas32com.public.blob.vercel-storage.com'],
+    },
 };
 
 export default nextConfig
