3a102db7cfb83914bf848d5b367a2576cd14842b
lihenko <obbdpu@gmail.com>
2025-08-17 16:18:19 +0300

    Smakota

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -33,6 +33,7 @@ const Footer: React.FC = () => {
                 <li><Link href="/">Головна</Link></li>
                 <li><Link href="/users">Наші кулінари</Link></li>
                 <li><Link href="/about">Про нас</Link></li>
+                <li><Link href="/privacy">Політика конфіденційності</Link></li>
               </ul>
             </nav>
           </div>
