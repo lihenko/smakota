744676b6c1b836ec40f0e7294ec8a0c59660d0d4
lihenko <obbdpu@gmail.com>
2025-08-17 15:50:19 +0300

    Smakota

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -26,14 +26,56 @@ const Footer: React.FC = () => {
             <h1 className="text-xl font-bold">Смакота</h1>
             <p className="font-bold">Твоя кулінарна книга онлайн</p>
           </div>
-          {/* Інші колонки вашого футера */}
+
+          <div className="px-4 w-1/2 mb-3 md:w-1/5">
+            <nav>
+              <ul>
+                <li><Link href="/">Головна</Link></li>
+                <li><Link href="/users">Наші кулінари</Link></li>
+                <li><Link href="/about">Про нас</Link></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="px-4 w-1/2 mb-3 md:w-1/5">
+            <nav>
+              <ul>
+                <li><Link href="/recipe/type/osnovni-stravy">Основні страви</Link></li>
+                <li><Link href="/recipe/type/yushky">Юшки</Link></li>   
+                <li><Link href="/recipe/type/garniry">Гарніри</Link></li>
+                <li><Link href="/recipe/type/salaty">Салати</Link></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="px-4 w-1/2 mb-3 md:w-1/5">
+            <nav>
+              <ul>
+                <li><Link href="/recipe/type/zakusky">Закуски</Link></li>
+                <li><Link href="/recipe/type/sousy">Соуси</Link></li>   
+                <li><Link href="/recipe/type/snidanok">Сніданок</Link></li>
+                <li><Link href="/recipe/type/deserty">Десерти</Link></li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="px-4 w-1/2 mb-3 md:w-1/5">
+            <nav>
+              <ul>
+                <li><Link href="/recipe/type/napoyi">Напої</Link></li>
+                <li><Link href="/recipe/type/vypichka">Випічка</Link></li>   
+                <li><Link href="/recipe/type/torty">Торти</Link></li>
+                <li><Link href="/recipe/type/konservatsiya">Консервація</Link></li>
+              </ul>
+            </nav>
+          </div>
         </div>
       </div>
 
-      {/* Кнопка Scroll to Top з анімацією */}
+      {/* Кнопка Scroll to Top */}
       <button
         onClick={scrollToTop}
-        className={`fixed cursor-pointer w-10 h-10 flex justify-center items-center bottom-8 right-8 bg-black text-white p-3 rounded-full shadow-lg 
+        className={`fixed cursor-pointer w-10 h-10 flex justify-center items-center bottom-8 right-8 bg-black text-white rounded-full shadow-lg 
           transition-opacity duration-500 ease-in-out 
           ${showButton ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}
       >
