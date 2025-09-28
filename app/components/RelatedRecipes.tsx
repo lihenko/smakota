2804de1971b3928c787ab73abb168216146ee766
lihenko <obbdpu@gmail.com>
2025-09-28 22:04:42 +0300

    Smakota

--- app/components/RelatedRecipes.tsx
+++ app/components/RelatedRecipes.tsx
@@ -101,6 +101,23 @@ export default async function RelatedRecipes({
                 <h2 className="text-lg font-semibold">Вакуумний пакувальник для кухні</h2>
               </div>
             </a>
+            <a
+              href={`/shop/smakuy-start-close`}
+              className="block bg-white rounded-xl shadow hover:shadow-md transition overflow-hidden relative"
+            >
+              <div className="relative w-full aspect-[3/2]">
+                <img
+                  src="/shop/start-15-l-zakrytyj-kub-700x700.jpg"
+                  alt='Самогонний апарат "Старт" на закритому кубі'
+                  className="object-cover"
+                  sizes="(max-width: 768px) 100vw, 33vw"
+                />
+              </div>
+
+              <div className="px-4 pt-4 pb-10">
+                <h2 className="text-lg font-semibold">Самогонний апарат "Старт" на закритому кубі</h2>
+              </div>
+            </a>
         </div>
         
       </div>
