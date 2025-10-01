3059cbc39e0d8525b7bbe6f5f6d9483b21f183a0
lihenko <obbdpu@gmail.com>
2025-10-01 12:26:53 +0300

    Smakota

---
+++ app/shop/smakuy-start-open/metadata.ts
@@ -0,0 +1,12 @@
+import { createMetadata } from "@/app/seo/createMetadata";
+
+export const metadata = {
+  ...createMetadata(
+    'Самогонний апарат «Старт» на відкритому кубі',
+    "Самогонний апарат «Старт» на відкритому кубі — компактний та надійний, з нержавіючої сталі, ідеальний для домашнього використання на будь-яких плитах.",
+    "shop/start-15-l-700x700-social.jpg",
+  ),
+  alternates: {
+    canonical: 'https://www.smakota.club/shop/smakuy-start-open',
+  },
+};
