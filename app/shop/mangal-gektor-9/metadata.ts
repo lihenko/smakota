08f476289ec0f19ad49d716f7c3b74c18501ed02
lihenko <obbdpu@gmail.com>
2025-10-03 16:48:53 +0300

    Smakota

--- app/shop/mangal-gektor-9/metadata.ts
+++ app/shop/mangal-gektor-9/metadata.ts
@@ -4,7 +4,7 @@ export const metadata = {
   ...createMetadata(
     'Мангал Гектор на 9 шампурів',
     "Мангал зі знімними ніжками для 9 шампурів з товщиною сталі 2, 3 або 4 мм. Міцний, зручний у транспортуванні та розрахований на довговічне використання вдома і на природі.",
-    "shop/SkovorodazBorony-social.jpg",
+    "shop/mangal-gektor-9-1.webp",
   ),
   alternates: {
     canonical: 'https://www.smakota.club/shop/mangal-gektor-9',
