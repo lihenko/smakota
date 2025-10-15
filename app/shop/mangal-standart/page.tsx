b46d2ff53464b76360d717f57bae65e9b9325ca7
lihenko <obbdpu@gmail.com>
2025-10-15 12:21:59 +0300

    Smakota

--- app/shop/mangal-standart/page.tsx
+++ app/shop/mangal-standart/page.tsx
@@ -6,9 +6,68 @@ export { metadata };
 
 export default function ShopPage() {
   const ProductName = "Мангал Стандарт";
+  const ShortDescription = "Мангал Стандарт — міцний та надійний мангал для дому та виїздів на природу. Компактний та зручний у транспортуванні, він дозволяє рівномірно готувати м’ясо, рибу та овочі.";
   const ProductPrice = 2699;
+  const ProductImage = 'https://www.smakota.club/shop/MangalStandart-700x700-social.png';
+  const InStock = true;
+  const productSchema = {
+    "@context": "https://schema.org/",
+    "@type": "Product",
+    "name": ProductName,
+    "image": ProductImage,
+    "description": ShortDescription,
+    "sku": "MANGAL-STANDART", // унікальний артикул, придумай свій
+    "brand": {
+      "@type": "Brand",
+      "name": "MZAVOD"
+    },
+    "offers": {
+      "@type": "Offer",
+      "url": "https://www.smakota.club/shop/mangal-standart",
+      "priceCurrency": "UAH",
+      "price": ProductPrice, // можна брати мінімальну або середню ціну
+      "availability": InStock
+        ? "https://schema.org/InStock"
+        : "https://schema.org/OutOfStock",
+      "itemCondition": "https://schema.org/NewCondition",
+      "shippingDetails": {
+        "@type": "OfferShippingDetails",
+        "shippingRate": {
+          "@type": "MonetaryAmount",
+          "value": "200.00",
+          "currency": "UAH"
+        },
+        "shippingDestination": {
+          "@type": "DefinedRegion",
+          "addressCountry": "UA"
+        },
+        "shippingLabel": "Нова Пошта",
+        "deliveryTime": {
+          "@type": "ShippingDeliveryTime",
+          "handlingTime": {
+            "@type": "QuantitativeValue",
+            "value": 2,
+            "minValue": 0,
+            "maxValue": 7,
+            "unitCode": "DAY"
+          },
+          "transitTime": {
+            "@type": "QuantitativeValue",
+            "value": 2,
+            "minValue": 2,
+            "maxValue": 3,
+            "unitCode": "DAY"
+          }
+        }
+      }
+    }   
+  };
   return (
     <main className="py-16">
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(productSchema) }}
+      />
       <div className="container max-w-5xl mx-auto">
         <div className="flex flex-wrap -mx-3 mb-6">
           <div className="w-full lg:w-1/2 px-3">
@@ -19,7 +78,7 @@ export default function ShopPage() {
           <div className="w-full lg:w-1/2 px-3 xl:pl-10">
             <h1 className="text-3xl font-bold mb-6 text-center lg:text-left">{ ProductName }</h1>
             <div className="text-lg mb-4">
-              Мангал Стандарт — міцний та надійний мангал для дому та виїздів на природу. Компактний та зручний у транспортуванні, він дозволяє рівномірно готувати м’ясо, рибу та овочі.
+              {ShortDescription}
             </div>
             <div className="text-lg mb-4">
               Ціна: <span className="font-bold">{ ProductPrice } грн</span>
