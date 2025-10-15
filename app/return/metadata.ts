abe913c9abf18b4e09dc173bef709577316fabf0
lihenko <obbdpu@gmail.com>
2025-10-15 17:21:58 +0300

    Smakota

--- app/return/metadata.ts
+++ app/return/metadata.ts
@@ -2,11 +2,11 @@ import { createMetadata } from "@/app/seo/createMetadata";
 
 export const metadata = {
   ...createMetadata(
-    "Політика конфіденційності – Смакота – Кращі домашні рецепти",
+    "Політика повернення і обміну – Смакота – Кращі домашні рецепти",
     "Smakota.club — український кулінарний сайт, де ви можете створити власну книгу рецептів, зберігати улюблені страви, ділитися ними та шукати рецепти за інгредієнтами. Усе — виключно українською мовою.",
     "/og-image.png"
   ),
   alternates: {
-    canonical: 'https://www.smakota.club/privacy',
+    canonical: 'https://www.smakota.club/return',
   },
 };
