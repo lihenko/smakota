f86853e71ad05a4b4a4665e79b174db75cfabcd9
lihenko <obbdpu@gmail.com>
2025-10-22 16:10:53 +0300

    Smakota

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -146,6 +146,23 @@ export default function ReturnPage() {
                   </div>
                 </Link>
                 <Link
+                  href={`/shop/autoclav-che-gas`}
+                  className="block bg-white rounded-xl shadow hover:shadow-md transition overflow-hidden relative"
+                >
+                  <div className="relative w-full aspect-[3/2]">
+                    <img
+                      src="/shop/che8gazikonka-1400x1400.webp"
+                      alt='Автоклав "ЧЕ" - Газовий'
+                      className="object-cover"
+                      sizes="(max-width: 768px) 100vw, 33vw"
+                    />
+                  </div>
+
+                  <div className="px-4 pt-4 pb-10">
+                    <h2 className="text-lg font-semibold">Автоклав "ЧЕ" - Газовий</h2>
+                  </div>
+                </Link>
+                <Link
                   href={`/shop/smakuy-baby-bum`}
                   className="block bg-white rounded-xl shadow hover:shadow-md transition overflow-hidden relative"
                 >
