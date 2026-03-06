4814cf0de4c0bb6ff48820f39db44e434854fbf6
lihenko <obbdpu@gmail.com>
2026-03-06 14:05:19 +0200

    Smakota

--- app/unauthorized/page.tsx
+++ app/unauthorized/page.tsx
@@ -5,6 +5,10 @@ export default async function Page() {
       <>
         <div className="text-center py-16">
           <h1>У вас недостатньо прав для доступа до цієї сторінки</h1>
+          <ul className="list-disc">
+            <li>Ви не авторизовані</li>
+            <li>У вас немає необхідних прав доступу</li>
+          </ul>
         </div>
       </>
     );
